8ffe7f9906e85c5d6a95e7d305802d29f029515a
Richard Garcia <code.richardy@gmail.com>
2025-08-01 22:13:18 -0300

    this commit introduces global usings for common Vinder IdentityProvider namespaces and AutoFixture in the test project.

--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -1,9 +1,15 @@
 /* global using for Vinder namespaces here */
 
+global using Vinder.IdentityProvider.Domain.Entities;
+global using Vinder.IdentityProvider.Domain.Filters.Builders;
+global using Vinder.IdentityProvider.Domain.Repositories;
+global using Vinder.IdentityProvider.Infrastructure.Repositories;
 global using Vinder.IdentityProvider.Infrastructure.IoC.Helpers;
+global using Vinder.IdentityProvider.TestSuite.IntegrationTests.Fixtures;
 
 /* global usings for third-party namespaces here */
 
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Containers;
 global using MongoDB.Driver;
+global using AutoFixture;
