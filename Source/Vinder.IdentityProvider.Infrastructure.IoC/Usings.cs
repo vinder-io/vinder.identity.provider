418f60b16582665f7c31adab3e62292e31c27638
Richard Garcia <code.richardy@gmail.com>
2025-08-01 21:47:19 -0300

    this commits introduces: Global using files for Infrastructure.IoC and Test projects to centralize common imports.

---
+++ Source/Vinder.IdentityProvider.Infrastructure.IoC/Usings.cs
@@ -0,0 +1,22 @@
+/* global usings for System namespaces here */
+
+global using System.Diagnostics.CodeAnalysis;
+
+/* global usings for Microsoft namespaces here */
+
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Hosting;
+
+/* global usings for Vinder namespaces here */
+
+global using Vinder.IdentityProvider.Domain.Entities;
+
+/* global usings for third-party namespaces here */
+
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization;
+global using MongoDB.Bson.Serialization.Serializers;
+global using MongoDB.Driver;
+global using FluentValidation;
+global using FluentValidation.AspNetCore;
---
+++ Tests/Usings.cs
@@ -0,0 +1,9 @@
+/* global using for Vinder namespaces here */
+
+global using Vinder.IdentityProvider.Infrastructure.IoC.Helpers;
+
+/* global usings for third-party namespaces here */
+
+global using DotNet.Testcontainers.Builders;
+global using DotNet.Testcontainers.Containers;
+global using MongoDB.Driver;
