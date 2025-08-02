da441f0bc9fb2bb5617b9da84dbcec17a20795b7
Richard Garcia <code.richardy@gmail.com>
2025-08-01 21:21:33 -0300

    this commit introduces global usings for domain filters, infrastructure components, and MongoDB BSON

--- Source/Vinder.IdentityProvider.Infrastructure/Usings.cs
+++ Source/Vinder.IdentityProvider.Infrastructure/Usings.cs
@@ -1,4 +1,14 @@
+/* global usings for Vinder namespaces here */
+
 global using Vinder.IdentityProvider.Domain.Entities;
 global using Vinder.IdentityProvider.Domain.Repositories;
+global using Vinder.IdentityProvider.Domain.Filters;
+
+global using Vinder.IdentityProvider.Infrastructure.Constants;
+global using Vinder.IdentityProvider.Infrastructure.Pipelines;
+
+/* global usings for third-party namespaces here */
 
 global using MongoDB.Driver;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization;
