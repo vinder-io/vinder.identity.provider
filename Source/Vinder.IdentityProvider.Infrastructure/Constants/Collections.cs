d9d60023b85079bda25b1a72692b23ed65a92c4c
Richard Garcia <code.richardy@gmail.com>
2025-08-01 21:01:15 -0300

    this commit introduces constants for database collection names in the Infrastructure layer

---
+++ Source/Vinder.IdentityProvider.Infrastructure/Constants/Collections.cs
@@ -0,0 +1,10 @@
+namespace Vinder.IdentityProvider.Infrastructure.Constants;
+
+public static class Collections
+{
+    public const string Users = "vinder.identity.users";
+    public const string Permissions = "vinder.identity.permissions";
+    public const string Groups = "vinder.identity.groups";
+    public const string Tokens = "vinder.identity.tokens";
+    public const string Tenants = "vinder.identity.tenants";
+}
