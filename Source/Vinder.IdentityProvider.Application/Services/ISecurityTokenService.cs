7e7119d510b6f8997ef0685f410b8291523afbca
Richard Garcia <code.richardy@gmail.com>
2025-08-03 16:28:34 -0300

    this commit introduces a refactor to the security token service interface, adjusting method signatures to use the security token entity and result objects for improved type safety and error handling.

--- Source/Vinder.IdentityProvider.Application/Services/ISecurityTokenService.cs
+++ Source/Vinder.IdentityProvider.Application/Services/ISecurityTokenService.cs
@@ -2,11 +2,28 @@ namespace Vinder.IdentityProvider.Application.Services;
 
 public interface ISecurityTokenService
 {
-    public Task<string> GenerateAccessTokenAsync(User user, CancellationToken cancellation = default);
-    public Task<string> GenerateRefreshTokenAsync(User user, CancellationToken cancellation = default);
+    public Task<Result<SecurityToken>> GenerateAccessTokenAsync(
+        User user,
+        CancellationToken cancellation = default
+    );
 
-    public Task<bool> ValidateAccessTokenAsync(string token, CancellationToken cancellation = default);
-    public Task<bool> ValidateRefreshTokenAsync(string token, CancellationToken cancellation = default);
+    public Task<Result<SecurityToken>> GenerateRefreshTokenAsync(
+        User user,
+        CancellationToken cancellation = default
+    );
 
-    public Task RevokeRefreshTokenAsync(string token, CancellationToken cancellation = default);
+    public Task<Result> ValidateAccessTokenAsync(
+        SecurityToken token,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> ValidateRefreshTokenAsync(
+        SecurityToken token,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> RevokeRefreshTokenAsync(
+        SecurityToken token,
+        CancellationToken cancellation = default
+    );
 }
