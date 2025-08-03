253672eff0c54f215a1f30c277d71d074cb60fee
Richard Garcia <code.richardy@gmail.com>
2025-08-03 15:29:09 -0300

    this commit introduces the token repository implementation

---
+++ Source/Vinder.IdentityProvider.Infrastructure/Repositories/TokenRepository.cs
@@ -0,0 +1,44 @@
+namespace Vinder.IdentityProvider.Infrastructure.Repositories;
+
+public sealed class TokenRepository(IMongoDatabase database) :
+    BaseRepository<SecurityToken>(database, Collections.Tokens),
+    ITokenRepository
+{
+    public async Task<IReadOnlyCollection<SecurityToken>> GetTokensAsync(TokenFilters filters, CancellationToken cancellation)
+    {
+        var pipeline = PipelineDefinitionBuilder
+            .For<SecurityToken>()
+            .As<SecurityToken, SecurityToken, BsonDocument>()
+            .FilterTokens(filters)
+            .Paginate(filters);
+
+        var options = new AggregateOptions { AllowDiskUse = true };
+        var aggregation = await _collection.AggregateAsync(pipeline, options, cancellation);
+
+        var bsonDocuments = await aggregation.ToListAsync(cancellation);
+        var tokens = bsonDocuments
+            .Select(bson => BsonSerializer.Deserialize<SecurityToken>(bson))
+            .ToList();
+
+        return tokens;
+    }
+
+    public async Task<long> CountAsync(TokenFilters filters, CancellationToken cancellation = default)
+    {
+        var pipeline = PipelineDefinitionBuilder
+            .For<SecurityToken>()
+            .As<SecurityToken, SecurityToken, BsonDocument>()
+            .FilterTokens(filters)
+            .Count();
+
+        var aggregation = await _collection.AggregateAsync(pipeline, cancellationToken: cancellation);
+        var result = await aggregation.FirstOrDefaultAsync(cancellation);
+
+        if (result == null)
+        {
+            return 0;
+        }
+
+        return result.Count;
+    }
+}
