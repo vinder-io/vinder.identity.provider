33b0fbb0bc544488d783a2a6fc01c98152d5e009
Richard Garcia <code.richardy@gmail.com>
2025-08-03 20:10:40 -0300

    this commit introduces new global usings for the infrastructure project

--- Source/Vinder.IdentityProvider.Infrastructure/Usings.cs
+++ Source/Vinder.IdentityProvider.Infrastructure/Usings.cs
@@ -1,11 +1,27 @@
+/* global usings for System namespaces here */
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Security.Cryptography;
+global using System.Text;
+
 /* global usings for Vinder namespaces here */
 
 global using Vinder.IdentityProvider.Domain.Entities;
 global using Vinder.IdentityProvider.Domain.Repositories;
 global using Vinder.IdentityProvider.Domain.Filters;
+global using Vinder.IdentityProvider.Domain.Filters.Builders;
 
 global using Vinder.IdentityProvider.Infrastructure.Constants;
 global using Vinder.IdentityProvider.Infrastructure.Pipelines;
+
+global using Vinder.IdentityProvider.Application.Services;
+
+global using Vinder.IdentityProvider.Common.Configuration;
+global using Vinder.IdentityProvider.Common.Errors;
+global using Vinder.IdentityProvider.Common.Results;
+
+global using SecurityToken = Vinder.IdentityProvider.Domain.Entities.SecurityToken;
 
 /* global usings for third-party namespaces here */
 
