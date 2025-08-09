a8297f68e1a84cfc52cacbe3a04e3a9c391f9896
Richard Garcia <code.richardy@gmail.com>
2025-08-09 15:28:46 -0300

    rename parameter 'cancellationToken' to 'cancellation' to match the interface declaration.sonarqube(csharpsquid:S927)

--- Source/Vinder.IdentityProvider.Infrastructure/Repositories/BaseRepository.cs
+++ Source/Vinder.IdentityProvider.Infrastructure/Repositories/BaseRepository.cs
@@ -5,34 +5,34 @@ public abstract class BaseRepository<TEntity>(IMongoDatabase database, string co
 {
     protected readonly IMongoCollection<TEntity> _collection = database.GetCollection<TEntity>(collection);
 
-    public virtual async Task<bool> DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
+    public virtual async Task<bool> DeleteAsync(TEntity entity, CancellationToken cancellation = default)
     {
         entity.MarkAsDeleted();
         entity.MarkAsUpdated();
 
         var filter = Builders<TEntity>.Filter.Eq(entity => entity.Id, entity.Id);
-        var result = await _collection.ReplaceOneAsync(filter, entity, cancellationToken: cancellationToken);
+        var result = await _collection.ReplaceOneAsync(filter, entity, cancellationToken: cancellation);
 
         return result.IsAcknowledged &&
                result.ModifiedCount > 0;
     }
 
-    public virtual async Task<TEntity> InsertAsync(TEntity entity, CancellationToken cancellationToken = default)
+    public virtual async Task<TEntity> InsertAsync(TEntity entity, CancellationToken cancellation = default)
     {
-        await _collection.InsertOneAsync(entity, cancellationToken: cancellationToken);
+        await _collection.InsertOneAsync(entity, cancellationToken: cancellation);
 
         entity.CreatedAt = DateTime.UtcNow;
 
         return entity;
     }
 
-    public virtual async Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+    public virtual async Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellation = default)
     {
         entity.MarkAsUpdated();
 
         var filter = Builders<TEntity>.Filter.Eq(entity => entity.Id, entity.Id);
 
-        await _collection.ReplaceOneAsync(filter, entity, cancellationToken: cancellationToken);
+        await _collection.ReplaceOneAsync(filter, entity, cancellationToken: cancellation);
         return entity;
     }
 }
