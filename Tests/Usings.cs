15572b8036bc75a29e10d84af8f1ae0960a99b5d
Richard Garcia <code.richardy@gmail.com>
2025-08-09 16:22:05 -0300

    this commit adds new global usings to the test project.

--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -3,8 +3,16 @@
 global using Vinder.IdentityProvider.Domain.Entities;
 global using Vinder.IdentityProvider.Domain.Filters.Builders;
 global using Vinder.IdentityProvider.Domain.Repositories;
+
+global using Vinder.IdentityProvider.Application.Services;
+global using Vinder.IdentityProvider.Application.Payloads.Identity;
+
 global using Vinder.IdentityProvider.Infrastructure.Repositories;
+global using Vinder.IdentityProvider.Infrastructure.Security;
 global using Vinder.IdentityProvider.Infrastructure.IoC.Helpers;
+
+global using Vinder.IdentityProvider.Common.Configuration;
+global using Vinder.IdentityProvider.Common.Errors;
 global using Vinder.IdentityProvider.TestSuite.IntegrationTests.Fixtures;
 
 /* global usings for third-party namespaces here */
