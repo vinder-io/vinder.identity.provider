5f34ca2068b2262ea23d616291a312033a7aeab1
Richard Garcia <code.richardy@gmail.com>
2025-08-09 15:45:11 -0300

    this commit adds MediatR and FluentValidation to the Application layer

--- Source/Vinder.IdentityProvider.Application/Usings.cs
+++ Source/Vinder.IdentityProvider.Application/Usings.cs
@@ -1,4 +1,10 @@
+/* global usings for Vinder namespaces here */
+
 global using Vinder.IdentityProvider.Application.Payloads.Identity;
 global using Vinder.IdentityProvider.Common.Results;
 global using Vinder.IdentityProvider.Domain.Entities;
 
+/* global usings for third-party namespaces here */
+
+global using FluentValidation;
+global using MediatR;
