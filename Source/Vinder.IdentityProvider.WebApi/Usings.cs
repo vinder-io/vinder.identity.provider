1799942684de031365073868efc7c996f94f3732
Richard Garcia <code.richardy@gmail.com>
2025-08-09 18:50:37 -0300

    this commit adds the global usings for the new identity controller.

--- Source/Vinder.IdentityProvider.WebApi/Usings.cs
+++ Source/Vinder.IdentityProvider.WebApi/Usings.cs
@@ -5,11 +5,18 @@ global using System.Text;
 
 /* global usings for Microsoft namespaces here */
 
-global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
 
 /* global usings for Vinder namespaces here */
 
-global using Vinder.IdentityProvider.Infrastructure.IoC.Extensions;
 global using Vinder.IdentityProvider.Common.Configuration;
+global using Vinder.IdentityProvider.Common.Errors;
+global using Vinder.IdentityProvider.Application.Payloads.Identity;
+global using Vinder.IdentityProvider.Infrastructure.IoC.Extensions;
 global using Vinder.IdentityProvider.WebApi.Extensions;
+
+/* global usings for third-party namespaces here */
+
+global using MediatR;
