f5eea9dcd2d3b91d5b651cdddf6fd6e6cd22daca
Richard Garcia <code.richardy@gmail.com>
2025-08-09 20:33:40 -0300

    this commit refactors tenant middleware to include caching and proper dependency injection.

--- Source/Vinder.IdentityProvider.WebApi/Middlewares/TenantMiddleware.cs
+++ Source/Vinder.IdentityProvider.WebApi/Middlewares/TenantMiddleware.cs
@@ -1,7 +1,20 @@
 namespace Vinder.IdentityProvider.WebApi.Middlewares;
 
-public sealed class TenantMiddleware(RequestDelegate next)
+public sealed class TenantMiddleware
 {
+    private readonly ITenantRepository _tenantRepository;
+    private readonly ITenantProvider _tenantProvider;
+    private readonly IMemoryCache _memoryCache;
+    private readonly RequestDelegate _next;
+
+    public TenantMiddleware(ITenantRepository repository, ITenantProvider provider, IMemoryCache memoryCache, RequestDelegate next)
+    {
+        _tenantRepository = repository;
+        _tenantProvider = provider;
+        _memoryCache = memoryCache;
+        _next = next;
+    }
+
     public async Task InvokeAsync(HttpContext context)
     {
         var tenantHeaderKey = context.Request.Headers.Keys
@@ -26,8 +39,47 @@ public sealed class TenantMiddleware(RequestDelegate next)
             return;
         }
 
-        context.Items["TenantName"] = context.Request.Headers[tenantHeaderKey].ToString();
+        var tenantName = context.Request.Headers[tenantHeaderKey].ToString();
+        var cacheKey = $"tenant:{tenantName}";
 
-        await next(context);
+        if (!_memoryCache.TryGetValue(cacheKey, out Tenant? tenant))
+        {
+            var filters = new TenantFiltersBuilder()
+                .WithName(tenantName)
+                .Build();
+
+            var tenants = await _tenantRepository.GetTenantsAsync(filters, context.RequestAborted);
+
+            tenant = tenants.FirstOrDefault();
+
+            if (tenant is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                context.Response.ContentType = MediaTypeNames.Application.Json;
+
+                var error = TenantErrors.TenantDoesNotExist;
+                var response = JsonSerializer.Serialize(new
+                {
+                    error = new
+                    {
+                        code = error.Code,
+                        description = error.Description
+                    }
+                });
+
+                await context.Response.WriteAsync(response);
+                return;
+            }
+
+            var cacheOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromDays(10));
+
+            _memoryCache.Set(cacheKey, tenant, cacheOptions);
+        }
+
+        context.Items["TenantName"] = tenant?.Name;
+
+        await _tenantProvider.SetTenantAsync(tenant!);
+        await _next(context);
     }
 }
