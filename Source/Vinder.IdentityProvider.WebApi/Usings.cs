0dcc58a47e52eed2981c3476485929f0284a9a01
Richard Garcia <code.richardy@gmail.com>
2025-08-09 20:25:09 -0300

    this commit adds necessary global usings to the WebApi project for caching, domain entities, filter builders, and repositories.

--- Source/Vinder.IdentityProvider.WebApi/Usings.cs
+++ Source/Vinder.IdentityProvider.WebApi/Usings.cs
@@ -8,16 +8,23 @@ global using System.Net.Mime;
 /* global usings for Microsoft namespaces here */
 
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Caching.Memory;
 global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.IdentityModel.Tokens;
 
 /* global usings for Vinder namespaces here */
 
+global using Vinder.IdentityProvider.Domain.Entities;
+global using Vinder.IdentityProvider.Domain.Filters.Builders;
+global using Vinder.IdentityProvider.Domain.Repositories;
+
 global using Vinder.IdentityProvider.Common.Configuration;
 global using Vinder.IdentityProvider.Common.Errors;
+
 global using Vinder.IdentityProvider.Application.Payloads.Identity;
 global using Vinder.IdentityProvider.Infrastructure.IoC.Extensions;
 global using Vinder.IdentityProvider.WebApi.Extensions;
+
 
 /* global usings for third-party namespaces here */
 
