2b312e9e78112e1710fc932e122067962469fa82
Richard Garcia <code.richardy@gmail.com>
2025-08-09 15:07:32 -0300

    this commit implements the ISettings interface on the Settings class

--- Source/Vinder.IdentityProvider.Common/Configuration/Settings.cs
+++ Source/Vinder.IdentityProvider.Common/Configuration/Settings.cs
@@ -1,6 +1,6 @@
 namespace Vinder.IdentityProvider.Common.Configuration;
 
-public sealed class Settings
+public sealed class Settings : ISettings
 {
     public DatabaseSettings Database { get; set; } = default!;
     public SecuritySettings Security { get; set; } = default!;
