a05381d91018eccec9d21be59a9e66b553078175
Richard Garcia <code.richardy@gmail.com>
2025-08-09 18:39:43 -0300

    this commit adds global usings for authentication and configuration to the WebApi project.

--- Source/Vinder.IdentityProvider.WebApi/Usings.cs
+++ Source/Vinder.IdentityProvider.WebApi/Usings.cs
@@ -1,4 +1,15 @@
+/* global usings for System namespaces here */
+
 global using System.Diagnostics.CodeAnalysis;
+global using System.Text;
+
+/* global usings for Microsoft namespaces here */
+
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+/* global usings for Vinder namespaces here */
 
 global using Vinder.IdentityProvider.Infrastructure.IoC.Extensions;
+global using Vinder.IdentityProvider.Common.Configuration;
 global using Vinder.IdentityProvider.WebApi.Extensions;
