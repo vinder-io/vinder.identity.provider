0023372db4a55a5f6ce12f44f63458f03a23ecd8
Richard Garcia <code.richardy@gmail.com>
2025-08-09 19:50:06 -0300

    this commit adds the TenantErrors class to define standardized error codes for tenant-related issues

---
+++ Source/Vinder.IdentityProvider.Common/Errors/TenantErrors.cs
@@ -0,0 +1,16 @@
+using Vinder.IdentityProvider.Common.Results;
+
+namespace Vinder.IdentityProvider.Common.Errors;
+
+public static class TenantErrors
+{
+    public static readonly Error HttpContextUnavailable = new(
+        Code: "#VINDER-IDP-ERR-TNT-500",
+        Description: "No HTTP context available to retrieve tenant information."
+    );
+
+    public static readonly Error TenantHeaderMissing = new(
+        Code: "#VINDER-IDP-ERR-TNT-400",
+        Description: "Tenant header is missing from the HTTP request."
+    );
+}
