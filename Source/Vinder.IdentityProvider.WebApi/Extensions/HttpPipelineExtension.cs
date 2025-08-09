67874067ce69a49bf12a94c9d547f928a855af5f
Richard Garcia <code.richardy@gmail.com>
2025-08-09 18:33:19 -0300

    this commit configures the HTTP pipeline, including middleware for HTTPS redirection, routing, CORS, authentication, and authorization.

---
+++ Source/Vinder.IdentityProvider.WebApi/Extensions/HttpPipelineExtension.cs
@@ -0,0 +1,21 @@
+namespace Vinder.IdentityProvider.WebApi.Extensions;
+
+[ExcludeFromCodeCoverage]
+public static class HttpPipelineExtension
+{
+    public static void UseHttpPipeline(this IApplicationBuilder app)
+    {
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseCors();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
