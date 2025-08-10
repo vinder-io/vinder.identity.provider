1ad03f6ae76172307b0c7e6f55438ea6228b90f9
Richard Garcia <code.richardy@gmail.com>
2025-08-10 14:57:49 -0300

    this commit introduces new permission constants for group management, including create, delete, and edit actions.

---
+++ Source/Vinder.IdentityProvider.Common/Constants/Permissions.cs
@@ -0,0 +1,8 @@
+namespace Vinder.IdentityProvider.Common.Constants;
+
+public static class Permissions
+{
+    public const string CreateGroup = "vinder.default.permissions.group.create";
+    public const string DeleteGroup = "vinder.default.permissions.group.delete";
+    public const string EditGroup = "vinder.default.permissions.group.update";
+}
