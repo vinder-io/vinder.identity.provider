348afeee76773746d0bdd39a2bb10d1bbf5ddf4b
Richard Garcia <code.richardy@gmail.com>
2025-08-10 02:54:17 -0300

    this commit adds global using for application services in WebApi

--- Source/Vinder.IdentityProvider.WebApi/Usings.cs
+++ Source/Vinder.IdentityProvider.WebApi/Usings.cs
@@ -23,6 +23,7 @@ global using Vinder.IdentityProvider.Common.Errors;
 
 global using Vinder.IdentityProvider.Application.Payloads.Identity;
 global using Vinder.IdentityProvider.Application.Providers;
+global using Vinder.IdentityProvider.Application.Services;
 
 global using Vinder.IdentityProvider.Infrastructure.IoC.Extensions;
 global using Vinder.IdentityProvider.WebApi.Extensions;
