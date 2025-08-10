6cd28102d6bdb4481b5cb3cc9e0276b0efa63ee6
Richard Garcia <code.richardy@gmail.com>
2025-08-10 18:36:02 -0300

    this commit introduces the client authentication credentials payload

---
+++ Source/Vinder.IdentityProvider.Application/Payloads/Identity/ClientAuthenticationCredentials.cs
@@ -0,0 +1,8 @@
+namespace Vinder.IdentityProvider.Application.Payloads.Identity;
+
+public sealed record ClientAuthenticationCredentials : IRequest<Result<ClientAuthenticationResult>>
+{
+    public string GrantType { get; init; } = default!;
+    public string ClientId { get; init; } = default!;
+    public string ClientSecret { get; init; } = default!;
+}
