9d812a9cf82d30309366dae133d76306106da4bd
Richard Garcia <code.richardy@gmail.com>
2025-08-10 02:36:07 -0300

    this commit reorganizes and adds global using statements in Vinder.IdentityProvider.Application/Usings.cs to centralize common namespaces and improve code consistency.

--- Source/Vinder.IdentityProvider.Application/Usings.cs
+++ Source/Vinder.IdentityProvider.Application/Usings.cs
@@ -1,9 +1,15 @@
 /* global usings for Vinder namespaces here */
 
+global using Vinder.IdentityProvider.Common.Results;
+global using Vinder.IdentityProvider.Common.Errors;
+
+global using Vinder.IdentityProvider.Domain.Entities;
+global using Vinder.IdentityProvider.Domain.Filters.Builders;
+global using Vinder.IdentityProvider.Domain.Repositories;
+
 global using Vinder.IdentityProvider.Application.Payloads.Identity;
 global using Vinder.IdentityProvider.Application.Services;
-global using Vinder.IdentityProvider.Common.Results;
-global using Vinder.IdentityProvider.Domain.Entities;
+global using Vinder.IdentityProvider.Application.Providers;
 
 /* global usings for third-party namespaces here */
 
