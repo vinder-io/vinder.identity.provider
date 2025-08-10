8076bde09d42caa692e5ac2f131fa682ce651a04
Richard Garcia <code.richardy@gmail.com>
2025-08-10 14:31:59 -0300

    this commit introduces the group creation handler, which is responsible for processing group creation requests, persisting new groups, and returning their details.

---
+++ Source/Vinder.IdentityProvider.Application/Handlers/Group/GroupForCreationHandler.cs
@@ -0,0 +1,15 @@
+namespace Vinder.IdentityProvider.Application.Handlers.Group;
+
+public sealed class GroupForCreationHandler(IGroupRepository groupRepository, ITenantProvider tenantProvider) :
+    IRequestHandler<GroupForCreation, Result<GroupDetails>>
+{
+    public async Task<Result<GroupDetails>> Handle(GroupForCreation request, CancellationToken cancellationToken)
+    {
+        var tenant = tenantProvider.GetCurrentTenant();
+        var group = GroupMapper.AsGroup(request, tenant);
+
+        await groupRepository.InsertAsync(group, cancellation: cancellationToken);
+
+        return Result<GroupDetails>.Success(GroupMapper.AsResponse(group));
+    }
+}
