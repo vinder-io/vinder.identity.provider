dcf390f6a8d4e1fb6219f035b214e0633e346844
Richard Garcia <code.richardy@gmail.com>
2025-08-10 18:36:59 -0300

    this commit adds the connect controller with the token endpoint for client authentication.

---
+++ Source/Vinder.IdentityProvider.WebApi/Controllers/ConnectController.cs
@@ -0,0 +1,27 @@
+namespace Vinder.IdentityProvider.WebApi.Controllers;
+
+[ApiController]
+[Route("api/v1/openid/connect")]
+public sealed class ConnectController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("token")]
+    public async Task<IActionResult> AuthenticateClientAsync(
+        [FromForm] ClientAuthenticationCredentials request,
+        CancellationToken cancellation
+    )
+    {
+        var result = await mediator.Send(request, cancellation);
+
+        return result switch
+        {
+            { IsSuccess: true } =>
+                StatusCode(StatusCodes.Status200OK, result.Data),
+
+            { IsFailure: true } when result.Error == AuthenticationErrors.ClientNotFound =>
+                StatusCode(StatusCodes.Status401Unauthorized, result.Error),
+
+            { IsFailure: true } when result.Error == AuthenticationErrors.InvalidClientCredentials =>
+                StatusCode(StatusCodes.Status401Unauthorized, result.Error),
+        };
+    }
+}
