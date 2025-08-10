c50f8f65bc34c443de83d6989048feab711bc7ac
Richard Garcia <code.richardy@gmail.com>
2025-08-10 17:55:50 -0300

    this commit introduces constants for identity claims.

---
+++ Source/Vinder.IdentityProvider.Infrastructure/Constants/IdentityClaimNames.cs
@@ -0,0 +1,8 @@
+namespace Vinder.IdentityProvider.Infrastructure.Constants;
+
+public static class IdentityClaimNames
+{
+    public const string ClientId = "client_id";
+    public const string TenantId = "tenant_id";
+    public const string TenantName = "tenant_name";
+}
