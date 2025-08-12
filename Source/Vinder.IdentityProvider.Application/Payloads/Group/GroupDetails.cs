6e0653e5df496ad3a63cf68134dede45d7239e3b
Richard Garcia <code.richardy@gmail.com>
2025-08-12 00:27:43 -0300

    this commit adds a Permissions property to the GroupDetails payload to include a list of associated permissions.

--- Source/Vinder.IdentityProvider.Application/Payloads/Group/GroupDetails.cs
+++ Source/Vinder.IdentityProvider.Application/Payloads/Group/GroupDetails.cs
@@ -4,4 +4,5 @@ public sealed record GroupDetails
 {
     public string Id { get; init; } = default!;
     public string Name { get; init; } = default!;
+    public IReadOnlyCollection<PermissionDetails> Permissions { get; init; } = [];
 }
