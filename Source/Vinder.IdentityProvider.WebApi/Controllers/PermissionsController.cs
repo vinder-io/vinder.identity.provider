23c5e18761dc5accdb81a23f7b191d9d32d9429f
Richard Garcia <code.richardy@gmail.com>
2025-08-12 21:37:40 -0300

    this commit adds the PermissionsController with an endpoint for creating new permissions. This includes authorization and conflict handling for existing permissions.

---
+++ Source/Vinder.IdentityProvider.WebApi/Controllers/PermissionsController.cs
@@ -0,0 +1,22 @@
+namespace Vinder.IdentityProvider.WebApi.Controllers;
+
+[ApiController]
+[Route("api/v1/permissions")]
+public sealed class PermissionsController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [Authorize(Roles = Permissions.CreatePermission)]
+    public async Task<IActionResult> CreatePermissionAsync(PermissionForCreation request, CancellationToken cancellation)
+    {
+        var result = await mediator.Send(request, cancellation);
+
+        return result switch
+        {
+            { IsSuccess: true } =>
+                StatusCode(StatusCodes.Status201Created, result.Data),
+
+            { IsFailure: true } when result.Error == PermissionErrors.PermissionAlreadyExists =>
+                StatusCode(StatusCodes.Status409Conflict, result.Error),
+        };
+    }
+}
