fd9b23f95dada720ce88e3a5e5b423ee5bcc4469
Richard Garcia <code.richardy@gmail.com>
2025-08-15 18:01:57 -0300

    this commit adds pagination support to the group and permission mappers.

--- Source/Vinder.IdentityProvider.Application/Mappers/GroupMapper.cs
+++ Source/Vinder.IdentityProvider.Application/Mappers/GroupMapper.cs
@@ -1,5 +1,3 @@
-using Vinder.IdentityProvider.Domain.Filters;
-
 namespace Vinder.IdentityProvider.Application.Mappers;
 
 public static class GroupMapper
@@ -22,6 +20,8 @@ public static class GroupMapper
         Id = parameters.Id,
         TenantId = parameters.TenantId,
         Name = parameters.Name,
+        PageNumber = parameters.PageNumber,
+        PageSize = parameters.PageSize,
         IsDeleted = parameters.IncludeDeleted.HasValue
             ? (bool?)(!parameters.IncludeDeleted.Value)
             : null
--- Source/Vinder.IdentityProvider.Application/Mappers/PermissionMapper.cs
+++ Source/Vinder.IdentityProvider.Application/Mappers/PermissionMapper.cs
@@ -8,6 +8,16 @@ public static class PermissionMapper
         Name = permission.Name
     };
 
+    public static PermissionFilters AsFilters(PermissionsFetchParameters parameters) => new()
+    {
+        Name = parameters.Name,
+        PageNumber = parameters.PageNumber,
+        PageSize = parameters.PageSize,
+        IsDeleted = parameters.IncludeDeleted.HasValue
+            ? (bool?)(!parameters.IncludeDeleted.Value)
+            : null
+    };
+
     public static Permission AsPermission(PermissionForCreation permission, Tenant tenant) => new()
     {
         Name = permission.Name,
