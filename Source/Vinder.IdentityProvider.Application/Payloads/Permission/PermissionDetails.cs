307d98b3b861cdd7381a6e7e06e8c8a3b9a23afe
Richard Garcia <code.richardy@gmail.com>
2025-08-15 18:41:54 -0300

    this commit adds the description property to the permission details payload

--- Source/Vinder.IdentityProvider.Application/Payloads/Permission/PermissionDetails.cs
+++ Source/Vinder.IdentityProvider.Application/Payloads/Permission/PermissionDetails.cs
@@ -4,4 +4,5 @@ public sealed record PermissionDetails
 {
     public string Id { get; init; } = default!;
     public string Name { get; init; } = default!;
+    public string? Description { get; init; } = default!;
 }
