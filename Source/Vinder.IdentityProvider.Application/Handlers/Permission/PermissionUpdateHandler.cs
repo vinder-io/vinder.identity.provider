779893fd93fd1b5da7e19472ce46d6137295c153
Richard Garcia <code.richardy@gmail.com>
2025-08-15 18:42:21 -0300

    this commit introduces the handler for updating a permission

---
+++ Source/Vinder.IdentityProvider.Application/Handlers/Permission/PermissionUpdateHandler.cs
@@ -0,0 +1,27 @@
+namespace Vinder.IdentityProvider.Application.Handlers.Permission;
+
+public sealed class PermissionUpdateHandler(IPermissionRepository repository, ITenantProvider tenantProvider) :
+    IRequestHandler<PermissionForUpdate, Result<PermissionDetails>>
+{
+    public async Task<Result<PermissionDetails>> Handle(PermissionForUpdate request, CancellationToken cancellationToken)
+    {
+        var tenant = tenantProvider.GetCurrentTenant();
+        var filters = new PermissionFiltersBuilder()
+            .WithName(request.Name)
+            .Build();
+
+        var permissions = await repository.GetPermissionsAsync(filters, cancellation: cancellationToken);
+        var permission = permissions.FirstOrDefault();
+
+        if (permission is null)
+        {
+            return Result<PermissionDetails>.Failure(PermissionErrors.PermissionDoesNotExist);
+        }
+
+        permission = PermissionMapper.AsPermission(request, permission, tenant);
+
+        var updatedPermission = await repository.UpdateAsync(permission, cancellation: cancellationToken);
+
+        return Result<PermissionDetails>.Success(PermissionMapper.AsResponse(updatedPermission));
+    }
+}
