f8fccd2b4913fb4e6b8194dc512c7cb9972a64f2
Richard Garcia <code.richardy@gmail.com>
2025-08-23 20:20:07 -0300

    this commit introduces default permissions for new tenants.

---
+++ Source/Vinder.IdentityProvider.Common/Constants/DefaultTenantPermissions.cs
@@ -0,0 +1,23 @@
+namespace Vinder.IdentityProvider.Common.Constants;
+
+public static class DefaultTenantPermissions
+{
+    public static readonly string[] InitialPermissions =
+    [
+        Permissions.CreateGroup,
+        Permissions.DeleteGroup,
+        Permissions.ViewGroups,
+        Permissions.EditGroup,
+
+        Permissions.DeleteUser,
+        Permissions.EditUser,
+        Permissions.ViewUsers,
+
+        Permissions.CreatePermission,
+        Permissions.AssignPermissions,
+        Permissions.RevokePermissions,
+        Permissions.ViewPermissions,
+        Permissions.EditPermission,
+        Permissions.DeletePermission
+    ];
+}
