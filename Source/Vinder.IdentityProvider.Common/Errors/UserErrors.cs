0f8f72926cdcaf48b2ed46b2c1731c756aba4912
Richard Garcia <code.richardy@gmail.com>
2025-08-23 19:19:59 -0300

    this commit adds new user-related error definitions for UserDoesNotExist and UserAlreadyInGroup to improve error handling within the IdentityProvider.

---
+++ Source/Vinder.IdentityProvider.Common/Errors/UserErrors.cs
@@ -0,0 +1,16 @@
+using Vinder.IdentityProvider.Common.Results;
+
+namespace Vinder.IdentityProvider.Common.Errors;
+
+public static class UserErrors
+{
+    public static readonly Error UserDoesNotExist = new(
+        Code: "#VINDER-IDP-ERR-USR-404",
+        Description: "The specified user does not exist. See https://bit.ly/errors-reference for more details."
+    );
+
+    public static readonly Error UserAlreadyInGroup = new(
+        Code: "#VINDER-IDP-ERR-USR-409",
+        Description: "The user is already a member of the specified group. See https://bit.ly/errors-reference for more details."
+    );
+}
