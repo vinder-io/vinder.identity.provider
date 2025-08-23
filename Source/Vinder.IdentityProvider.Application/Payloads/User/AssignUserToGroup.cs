18ed6d37f74f48b1dc33ba87d8f34fb0529c5c95
Richard Garcia <code.richardy@gmail.com>
2025-08-23 19:28:14 -0300

    this commit adds [JsonIgnore] to the UserId property in AssignUserToGroup payload to prevent its serialization.

--- Source/Vinder.IdentityProvider.Application/Payloads/User/AssignUserToGroup.cs
+++ Source/Vinder.IdentityProvider.Application/Payloads/User/AssignUserToGroup.cs
@@ -2,6 +2,7 @@ namespace Vinder.IdentityProvider.Application.Payloads.User;
 
 public sealed record AssignUserToGroup : IRequest<Result>
 {
+    [JsonIgnore]
     public Guid UserId { get; init; }
     public Guid GroupId { get; init; }
 }
