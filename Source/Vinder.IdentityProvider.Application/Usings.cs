3a6f40058f60a51f39e535acf63465226d4b9f38
Richard Garcia <code.richardy@gmail.com>
2025-08-23 18:39:30 -0300

    this commit adds a global using directive for the User payloads namespace.

--- Source/Vinder.IdentityProvider.Application/Usings.cs
+++ Source/Vinder.IdentityProvider.Application/Usings.cs
@@ -18,6 +18,7 @@ global using Vinder.IdentityProvider.Application.Payloads.Identity;
 global using Vinder.IdentityProvider.Application.Payloads.Group;
 global using Vinder.IdentityProvider.Application.Payloads.Permission;
 global using Vinder.IdentityProvider.Application.Payloads.Tenant;
+global using Vinder.IdentityProvider.Application.Payloads.User;
 
 global using Vinder.IdentityProvider.Application.Services;
 global using Vinder.IdentityProvider.Application.Providers;
