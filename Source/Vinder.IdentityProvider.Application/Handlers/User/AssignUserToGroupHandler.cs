b41da38ca3c7c881968e27f513ed9fa0c3c54503
Richard Garcia <code.richardy@gmail.com>
2025-08-23 19:20:42 -0300

    this commit implements the AssignUserToGroupHandler to manage user-to-group assignments, including validation and permission merging.

---
+++ Source/Vinder.IdentityProvider.Application/Handlers/User/AssignUserToGroupHandler.cs
@@ -0,0 +1,54 @@
+namespace Vinder.IdentityProvider.Application.Handlers.User;
+
+public sealed class AssignUserToGroupHandler(IUserRepository userRepository, IGroupRepository groupRepository) :
+    IRequestHandler<AssignUserToGroup, Result>
+{
+    public async Task<Result> Handle(AssignUserToGroup request, CancellationToken cancellationToken)
+    {
+        var userFilters = new UserFiltersBuilder()
+            .WithUserId(request.UserId)
+            .Build();
+
+        var matchingUsers = await userRepository.GetUsersAsync(userFilters, cancellationToken);
+        var existingUser = matchingUsers.FirstOrDefault();
+
+        if (existingUser is null)
+        {
+            return Result.Failure(UserErrors.UserDoesNotExist);
+        }
+
+        var groupFilters = new GroupFiltersBuilder()
+            .WithId(request.GroupId)
+            .Build();
+
+        var matchingGroups = await groupRepository.GetGroupsAsync(groupFilters, cancellationToken);
+        var existingGroup = matchingGroups.FirstOrDefault();
+
+        if (existingGroup is null)
+        {
+            return Result.Failure(GroupErrors.GroupDoesNotExist);
+        }
+
+        if (existingUser.Groups.Any(group => group.Id == request.GroupId))
+        {
+            return Result.Failure(UserErrors.UserAlreadyInGroup);
+        }
+
+        existingUser.Groups.Add(existingGroup);
+
+        var groupPermissions = existingGroup.Permissions ?? [  ];
+        var userPermissions = existingUser.Permissions ?? [  ];
+
+        var newPermissions = groupPermissions
+            .Where(permission => userPermissions.All(existingPermission => existingPermission.Name != permission.Name))
+            .ToList();
+
+        existingUser.Permissions = userPermissions
+            .Concat(newPermissions)
+            .ToList();
+
+        await userRepository.UpdateAsync(existingUser, cancellationToken);
+
+        return Result.Success();
+    }
+}
