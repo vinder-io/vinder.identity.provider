832488ddb6feb49ca0bfa8c59d563557416df21a
Richard Garcia <code.richardy@gmail.com>
2025-08-23 16:15:39 -0300

    this commit adds a new endpoint to the TenantsController for fetching tenants with pagination and filtering.

--- Source/Vinder.IdentityProvider.WebApi/Controllers/TenantsController.cs
+++ Source/Vinder.IdentityProvider.WebApi/Controllers/TenantsController.cs
@@ -4,6 +4,20 @@ namespace Vinder.IdentityProvider.WebApi.Controllers;
 [Route("api/v1/tenants")]
 public sealed class TenantsController(IMediator mediator) : ControllerBase
 {
+    [HttpGet]
+    [Authorize(Roles = Permissions.ViewTenants)]
+    public async Task<IActionResult> GetTenantsAsync([FromQuery] TenantFetchParameters request, CancellationToken cancellation)
+    {
+        var result = await mediator.Send(request, cancellation);
+
+        // we know the switch here is not strictly necessary since we only handle the success case,
+        // but we keep it for consistency with the rest of the codebase and to follow established patterns.
+        return result switch
+        {
+            { IsSuccess: true } => StatusCode(StatusCodes.Status200OK, result.Data),
+        };
+    }
+
     [HttpPost]
     [Authorize(Roles = Permissions.CreateTenant)]
     public async Task<IActionResult> CreateTenantAsync(TenantForCreation request, CancellationToken cancellation)
