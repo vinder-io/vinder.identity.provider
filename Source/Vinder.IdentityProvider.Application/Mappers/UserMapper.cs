1d7195de321441300f7ca7b72d620a7bb1d004c7
Richard Garcia <code.richardy@gmail.com>
2025-08-23 18:39:49 -0300

    this commit extends the UserMapper to include mappings for user filters and user details payloads.

--- Source/Vinder.IdentityProvider.Application/Mappers/UserMapper.cs
+++ Source/Vinder.IdentityProvider.Application/Mappers/UserMapper.cs
@@ -5,6 +5,23 @@ public static class UserMapper
     public static User AsUser(IdentityEnrollmentCredentials credentials, Guid tenantId) => new()
     {
         Username = credentials.Username,
-        TenantId = tenantId
+        TenantId = tenantId,
+    };
+
+    public static UserFilters AsFilters(UsersFetchParameters parameters) => new()
+    {
+        UserId = parameters.Id,
+        Username = parameters.Username,
+        IsDeleted = parameters.IsDeleted,
+        PageNumber = parameters.PageNumber,
+        PageSize = parameters.PageSize
+    };
+
+    public static UserDetails AsResponse(User user) => new()
+    {
+        Id = user.Id.ToString(),
+        Username = user.Username,
+        Groups = [.. user.Groups.Select(GroupMapper.AsResponse)],
+        Permissions = [.. user.Permissions.Select(PermissionMapper.AsResponse)]
     };
 }
