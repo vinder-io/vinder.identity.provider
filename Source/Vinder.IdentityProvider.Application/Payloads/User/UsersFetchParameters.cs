cacffed5e07d1b1c9402aa9f3940037faae1fa14
Richard Garcia <code.richardy@gmail.com>
2025-08-23 18:39:12 -0300

    this commit introduces the UsersFetchParameters payload for fetching and filtering user data with pagination.

---
+++ Source/Vinder.IdentityProvider.Application/Payloads/User/UsersFetchParameters.cs
@@ -0,0 +1,10 @@
+namespace Vinder.IdentityProvider.Application.Payloads.User;
+
+public sealed record UsersFetchParameters : IRequest<Result<Pagination<UserDetails>>>
+{
+    public Guid? Id { get; init; }
+    public string? Username { get; init; }
+    public bool? IsDeleted { get; set; }
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 20;
+}
