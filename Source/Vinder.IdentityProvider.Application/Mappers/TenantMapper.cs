2191d763237da1ba544818965a27d821ddf0955d
Richard Garcia <code.richardy@gmail.com>
2025-08-23 15:27:47 -0300

    this commit introduces the tenant mapper, which is responsible for mapping tenant-related objects.

---
+++ Source/Vinder.IdentityProvider.Application/Mappers/TenantMapper.cs
@@ -0,0 +1,20 @@
+namespace Vinder.IdentityProvider.Application.Mappers;
+
+public static class TenantMapper
+{
+    public static Tenant AsTenant(TenantForCreation tenant, string clientId, string secretHash) => new()
+    {
+        Name = tenant.Name,
+        Description = tenant.Description,
+        ClientId = clientId,
+        SecretHash = secretHash
+    };
+
+    public static TenantDetails AsResponse(Tenant tenant) => new()
+    {
+        Name = tenant.Name,
+        Description = tenant.Description,
+        ClientId = tenant.ClientId,
+        ClientSecret = tenant.SecretHash
+    }; 
+}
