d4d1dae5a611619f5be0d8b44a40baa00bf56093
Richard Garcia <code.richardy@gmail.com>
2025-08-23 19:56:08 -0300

    this commit renames permission constants from vinder.default to vinder.defaults and updates user-related permissions, removing client-specific ones.

--- Source/Vinder.IdentityProvider.Common/Constants/Permissions.cs
+++ Source/Vinder.IdentityProvider.Common/Constants/Permissions.cs
@@ -2,25 +2,24 @@ namespace Vinder.IdentityProvider.Common.Constants;
 
 public static class Permissions
 {
-    public const string CreateGroup = "vinder.default.permissions.group.create";
-    public const string DeleteGroup = "vinder.default.permissions.group.delete";
-    public const string EditGroup = "vinder.default.permissions.group.update";
-    public const string ViewGroups = "vinder.default.permissions.group.view";
+    public const string CreateGroup = "vinder.defaults.permissions.group.create";
+    public const string DeleteGroup = "vinder.defaults.permissions.group.delete";
+    public const string EditGroup = "vinder.defaults.permissions.group.update";
+    public const string ViewGroups = "vinder.defaults.permissions.group.view";
 
-    public const string CreateClient = "vinder.default.permissions.client.create";
-    public const string DeleteClient = "vinder.default.permissions.client.delete";
-    public const string EditClient = "vinder.default.permissions.client.update";
-    public const string ViewClients = "vinder.default.permissions.client.view";
+    public const string DeleteUser = "vinder.defaults.permissions.user.delete";
+    public const string EditUser = "vinder.defaults.permissions.user.update";
+    public const string ViewUsers = "vinder.defaults.permissions.user.view";
 
-    public const string CreatePermission = "vinder.default.permissions.permissions.create";
-    public const string AssignPermissions = "vinder.default.permissions.permissions.assign";
-    public const string RevokePermissions = "vinder.default.permissions.permissions.revoke";
-    public const string ViewPermissions = "vinder.default.permissions.permissions.view";
-    public const string EditPermission = "vinder.default.permissions.permissions.edit";
-    public const string DeletePermission = "vinder.default.permissions.permissions.delete";
+    public const string CreatePermission = "vinder.defaults.permissions.permissions.create";
+    public const string AssignPermissions = "vinder.defaults.permissions.permissions.assign";
+    public const string RevokePermissions = "vinder.defaults.permissions.permissions.revoke";
+    public const string ViewPermissions = "vinder.defaults.permissions.permissions.view";
+    public const string EditPermission = "vinder.defaults.permissions.permissions.edit";
+    public const string DeletePermission = "vinder.defaults.permissions.permissions.delete";
 
-    public const string CreateTenant = "vinder.default.permissions.tenant.create";
-    public const string DeleteTenant = "vinder.default.permissions.tenant.delete";
-    public const string EditTenant = "vinder.default.permissions.tenant.update";
-    public const string ViewTenants = "vinder.default.permissions.tenant.view";
+    public const string CreateTenant = "vinder.defaults.permissions.tenant.create";
+    public const string DeleteTenant = "vinder.defaults.permissions.tenant.delete";
+    public const string EditTenant = "vinder.defaults.permissions.tenant.update";
+    public const string ViewTenants = "vinder.defaults.permissions.tenant.view";
 }
