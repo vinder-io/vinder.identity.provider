78ab08b6f1dfb213f961f5dc82d57194257fff84
Richard Garcia <code.richardy@gmail.com>
2025-08-23 15:04:58 -0300

    this commit introduces a validator for tenant creation. It ensures that the tenant name is not empty, has a minimum length of 3 and a maximum length of 100 characters.

---
+++ Source/Vinder.IdentityProvider.Application/Validators/Tenant/TenantCreationValidator.cs
@@ -0,0 +1,20 @@
+namespace Vinder.IdentityProvider.Application.Validators.Tenant;
+
+public sealed class TenantCreationValidator : AbstractValidator<TenantForCreation>
+{
+    public TenantCreationValidator()
+    {
+        RuleFor(tenant => tenant.Name)
+            .NotEmpty()
+            .WithMessage("Tenant name must not be empty.")
+            .MinimumLength(3)
+            .WithMessage("Tenant name must be at least 3 characters long.")
+            .MaximumLength(100)
+            .WithMessage("Tenant name must be at most 100 characters long.");
+
+        RuleFor(tenant => tenant.Description)
+            .MaximumLength(500)
+            .WithMessage("Tenant description must be at most 500 characters long.")
+            .When(tenant => !string.IsNullOrEmpty(tenant.Description));
+    }
+}
