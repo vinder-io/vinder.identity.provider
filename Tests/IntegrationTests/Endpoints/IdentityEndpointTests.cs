11710711504a5969dc6235f7b5a2aef320362047
Richard Garcia <code.richardy@gmail.com>
2025-08-24 10:12:44 -0300

    this commit adds integration tests for the identity endpoint

---
+++ Tests/IntegrationTests/Endpoints/IdentityEndpointTests.cs
@@ -0,0 +1,35 @@
+namespace Vinder.IdentityProvider.TestSuite.IntegrationTests.Endpoints;
+
+public sealed class IdentityEndpointTests(WebApplicationFixture factory) :
+    IClassFixture<WebApplicationFixture>,
+    IAsyncLifetime
+{
+    [Fact(DisplayName = "[e2e] - when POST identity/authenticate with valid credentials should return access token 'n refresh token")]
+    public async Task WhenPostIdentityAuthenticateWithValidCredentials_ShouldReturnAccessTokenAndRefreshToken()
+    {
+        /* arrange: define credentials for an existing user */
+        var httpClient = factory.HttpClient.WithTenantHeader("master");
+        var credentials = new AuthenticationCredentials
+        {
+            Username = "vinder.testing.user",
+            Password = "vinder.testing.password"
+        };
+
+        /* act: send POST request to authenticate endpoint */
+        var response = await httpClient.PostAsJsonAsync("api/v1/identity/authenticate", credentials);
+        var result = await response.Content.ReadFromJsonAsync<AuthenticationResult>();
+
+        /* assert: response and result should not be null, and status code should be OK */
+        Assert.NotNull(response);
+        Assert.NotNull(result);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        /* assert: both access token and refresh token should be returned and not empty */
+        Assert.NotEmpty(result.AccessToken);
+        Assert.NotEmpty(result.RefreshToken);
+    }
+
+    public async Task DisposeAsync() => await factory.DisposeAsync();
+    public async Task InitializeAsync() => await factory.InitializeAsync();
+}
