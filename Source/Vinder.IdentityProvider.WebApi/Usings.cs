fcba0afd9bfa6a0ba73ea5f716fcc0421217b85e
Richard Garcia <code.richardy@gmail.com>
2025-08-25 21:18:18 -0300

    chore: this commit reordering the using statements to force pipeline

--- Source/Vinder.IdentityProvider.WebApi/Usings.cs
+++ Source/Vinder.IdentityProvider.WebApi/Usings.cs
@@ -26,8 +26,8 @@ global using Vinder.IdentityProvider.Common.Constants;
 global using Vinder.IdentityProvider.Common.Configuration;
 global using Vinder.IdentityProvider.Common.Errors;
 
-global using Vinder.IdentityProvider.Application.Payloads.Identity;
 global using Vinder.IdentityProvider.Application.Payloads.Group;
+global using Vinder.IdentityProvider.Application.Payloads.Identity;
 global using Vinder.IdentityProvider.Application.Payloads.Permission;
 global using Vinder.IdentityProvider.Application.Payloads.Tenant;
 global using Vinder.IdentityProvider.Application.Payloads.User;
