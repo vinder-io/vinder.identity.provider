969705a5a565f502a0d9cfa4a38f40cfb5382283
Richard Garcia <code.richardy@gmail.com>
2025-08-25 21:15:14 -0300

    this commit removes the tenant property from the administration configuration

--- Source/Vinder.IdentityProvider.Common/Configuration/Administration.cs
+++ Source/Vinder.IdentityProvider.Common/Configuration/Administration.cs
@@ -4,5 +4,4 @@ public sealed class Administration
 {
     public string Username { get; set; } = default!;
     public string Password { get; set; } = default!;
-    public string Tenant { get; set; } = default!;
 }
