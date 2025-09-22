3866e3de9d0faa1a3f7bdda57edd461a8928312a
Richard Garcia <code.richardy@gmail.com>
2025-09-22 19:11:46 -0300

    this commit adds the scopes collection constant

--- Source/Vinder.IdentityProvider.Infrastructure/Constants/Collections.cs
+++ Source/Vinder.IdentityProvider.Infrastructure/Constants/Collections.cs
@@ -7,4 +7,5 @@ public static class Collections
     public const string Groups = "vinder.identity.groups";
     public const string Tokens = "vinder.identity.tokens";
     public const string Tenants = "vinder.identity.tenants";
+    public const string Scopes = "vinder.identity.scopes";
 }
