49c66de76ddb51fd77ec509c05e0640f23f07db5
Richard Garcia <code.richardy@gmail.com>
2025-09-23 22:32:38 -0300

    introduces the class of scope-related errors

---
+++ Source/Vinder.IdentityProvider.Common/Errors/ScopeErrors.cs
@@ -0,0 +1,14 @@
+namespace Vinder.IdentityProvider.Common.Errors;
+
+public static class ScopeErrors
+{
+    public static readonly Error ScopeAlreadyExists = new(
+        Code: "#VINDER-IDP-ERROR-8D128",
+        Description: "The scope with the specified name already exists. See https://bit.ly/errors-reference for more details."
+    );
+
+    public static readonly Error ScopeDoesNotExists = new(
+        Code: "#VINDER-IDP-ERROR-903F9",
+        Description: "The scope with the specified name does not exist. See https://bit.ly/errors-reference for more details."
+    );
+}
