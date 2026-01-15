72893869e06fb58043e21cc377cb328b5e9b5d33
Richard Garcia <code.richardy@gmail.com>
2026-01-15 11:41:25 -0300

    feat: introduces specifications extension for mapping scalar api reference in vinder identity webapi

---
+++ Source/Vinder.Identity.WebApi/Extensions/SpecificationExtension.cs
@@ -0,0 +1,21 @@
+namespace Vinder.Identity.WebApi.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "contains only dependency injection registration with no business logic.")]
+public static class SpecificationsExtension
+{
+    public static void UseSpecification(this IEndpointRouteBuilder app)
+    {
+        app.MapScalarApiReference(options =>
+        {
+            options.DarkMode = false;
+            options.HideDarkModeToggle = true;
+            options.HideClientButton = true;
+            options.HideModels = true;
+            options.HideSearch = true;
+
+            options.WithTitle("Vinder Identity | Reference");
+            options.WithClassicLayout();
+            options.ExpandAllTags();
+        });
+    }
+}
