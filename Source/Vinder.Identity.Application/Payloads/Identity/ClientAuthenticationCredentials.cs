f8f0e3b10b145fd78c22391608918687f3031547
Richard Garcia <code.richardy@gmail.com>
2026-01-17 14:03:56 -0300

    feat: this commit introduces properties for authorization_code grant type in client authentication credentials

--- Source/Vinder.Identity.Application/Payloads/Identity/ClientAuthenticationCredentials.cs
+++ Source/Vinder.Identity.Application/Payloads/Identity/ClientAuthenticationCredentials.cs
@@ -3,6 +3,12 @@ namespace Vinder.Identity.Application.Payloads.Identity;
 public sealed record ClientAuthenticationCredentials : IMessage<Result<ClientAuthenticationResult>>
 {
     public string GrantType { get; init; } = default!;
+
+    // for client_credentials grant type
     public string ClientId { get; init; } = default!;
     public string ClientSecret { get; init; } = default!;
+
+    // for authorization_code grant type
+    public string Code { get; init; } = default!;
+    public string CodeVerifier { get; init; } = default!;
 }
