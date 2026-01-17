d2e72cdace20bbe722cf73924fe9c90598ad539c
Richard Garcia <code.richardy@gmail.com>
2026-01-17 16:27:53 -0300

    feat: this commit introduces authorization page with form handling for user credentials and redirect uri

---
+++ Source/Vinder.Identity.WebApi/Pages/Authorize.cshtml.cs
@@ -0,0 +1,38 @@
+namespace Vinder.Identity.WebApi.Pages;
+
+public sealed class AuthorizePage(IDispatcher dispatcher, ITenantCollection tenantCollection, ITenantProvider tenantProvider) : PageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public AuthorizationParameters Parameters { get; set; } = new();
+
+    [BindProperty]
+    public AuthenticationCredentials Credentials { get; set; } = new();
+
+    public async Task<IActionResult> OnGetAsync() => Page();
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var filters = new TenantFiltersBuilder()
+            .WithClientId(Parameters.ClientId)
+            .Build();
+
+        var tenants = await tenantCollection.GetTenantsAsync(filters);
+        var tenant = tenants.FirstOrDefault();
+
+        if (tenant is null)
+        {
+            ModelState.AddModelError(TenantErrors.TenantDoesNotExist.Code, TenantErrors.TenantDoesNotExist.Description);
+            return Page();
+        }
+
+        tenantProvider.SetTenant(tenant);
+
+        var result = await dispatcher.DispatchAsync(Credentials);
+        if (result.IsFailure)
+        {
+            ModelState.AddModelError(AuthenticationErrors.InvalidCredentials.Code, result.Error.Description);
+            return Page();
+        }
+
+        return Redirect($"{Parameters.RedirectUri}?code=CODE&state={Parameters.State}");
+    }
+}
