20cd3b6ffaae0dc6578d2cb7e4371ca70e2a2a5b
Richard Garcia <code.richardy@gmail.com>
2026-01-17 22:17:53 -0300

    fix: update collection constants to reflect project renaming

--- Source/Vinder.Federation.Infrastructure/Constants/Collections.cs
+++ Source/Vinder.Federation.Infrastructure/Constants/Collections.cs
@@ -2,11 +2,11 @@ namespace Vinder.Federation.Infrastructure.Constants;
 
 public static class Collections
 {
-    public const string Users = "vinder.identity.users";
-    public const string Permissions = "vinder.identity.permissions";
-    public const string Groups = "vinder.identity.groups";
-    public const string Tokens = "vinder.identity.tokens";
-    public const string Tenants = "vinder.identity.tenants";
-    public const string Scopes = "vinder.identity.scopes";
-    public const string Secrets = "vinder.identity.secrets";
+    public const string Users = "vinder.federation.users";
+    public const string Permissions = "vinder.federation.permissions";
+    public const string Groups = "vinder.federation.groups";
+    public const string Tokens = "vinder.federation.tokens";
+    public const string Tenants = "vinder.federation.tenants";
+    public const string Scopes = "vinder.federation.scopes";
+    public const string Secrets = "vinder.federation.secrets";
 }
