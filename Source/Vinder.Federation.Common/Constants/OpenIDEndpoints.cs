4d101d1fb93329a9efe6bf4d7efadfb51c9be8d2
Richard Garcia <code.richardy@gmail.com>
2026-01-17 22:30:44 -0300

    fix: updates open id token endpoint to correct protocol path

--- Source/Vinder.Federation.Common/Constants/OpenIDEndpoints.cs
+++ Source/Vinder.Federation.Common/Constants/OpenIDEndpoints.cs
@@ -3,7 +3,7 @@ namespace Vinder.Federation.Common.Constants;
 public static class OpenIDEndpoints
 {
     public const string Authorize = "api/v1/openid/authorize";
-    public const string Token = "api/v1/openid/connect/token";
+    public const string Token = "api/v1/protocol/open-id/connect/token";
     public const string UserInfo = "api/v1/openid/user-info";
     public const string Jwks = ".well-known/jwks.json";
 }
