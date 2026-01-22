faeaaff9577430002437349fc042ee8eea5bd4da
Richard Garcia <code.richardy@gmail.com>
2026-01-21 23:25:50 -0300

    feat: implement authorization handler for authorization parameters validation and redirect uri enforcement

---
+++ Source/Vinder.Federation.Application/Handlers/Authorization/AuthorizationHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Vinder.Federation.Application.Handlers.Authorization;
+
+public sealed class AuthorizationHandler(ITenantCollection tenantCollection, IRedirectUriPolicy redirectUriPolicy) :
+    IMessageHandler<AuthorizationParameters, Result<AuthorizationScheme>>
+{
+    public async Task<Result<AuthorizationScheme>> HandleAsync(
+        AuthorizationParameters parameters, CancellationToken cancellation = default)
+    {
+        var filters = new TenantFiltersBuilder()
+            .WithClientId(parameters.ClientId)
+            .Build();
+
+        var clients = await tenantCollection.GetTenantsAsync(filters, cancellation);
+        var client = clients.FirstOrDefault();
+
+        if (client is null)
+        {
+            return Result<AuthorizationScheme>.Failure(TenantErrors.TenantDoesNotExist);
+        }
+
+        var redirectProof = await redirectUriPolicy.EnsureRedirectUriIsAllowedAsync(client, redirectUri, cancellation);
+        if (redirectProof.IsFailure)
+        {
+            return Result<AuthorizationScheme>.Failure(redirectProof.Error);
+        }
+
+        return Result<AuthorizationScheme>.Success(parameters.AsReponse());
+    }
+}
