dd7623a15ab405edcaeb748f425cfbbc3922cdda
Richard Garcia <code.richardy@gmail.com>
2026-01-22 00:26:11 -0300

    fix: fix global using syntax for vinder.federation.application.payloads.permission

--- Source/Vinder.Federation.Infrastructure.IoC/Usings.cs
+++ Source/Vinder.Federation.Infrastructure.IoC/Usings.cs
@@ -15,7 +15,7 @@ global using Vinder.Federation.Application.Providers;
 global using Vinder.Federation.Application.Payloads.Identity;
 global using Vinder.Federation.Application.Payloads.Authorization;
 global using Vinder.Federation.Application.Payloads.Group;
-global using Vinder.Federation.Application.Payloads.Permission
+global using Vinder.Federation.Application.Payloads.Permission;
 global using Vinder.Federation.Application.Payloads.Tenant;
 global using Vinder.Federation.Application.Payloads.User;
 global using Vinder.Federation.Application.Payloads.Scope;
