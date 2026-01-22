e111c15e554edf09807306a5a74896e4181408e8
Richard Garcia <code.richardy@gmail.com>
2026-01-21 22:51:19 -0300

    feat: this commit introduces records for parameters and authorization scheme

---
+++ Source/Vinder.Federation.Application/Payloads/Authorization/AuthorizationScheme.cs
@@ -0,0 +1,10 @@
+﻿namespace Vinder.Federation.Application.Payloads.Authorization;
+
+public sealed record AuthorizationScheme
+{
+    public string ClientId { get; init; } = default!;
+    public string RedirectUri { get; init; } = default!;
+    public string CodeChallenge { get; init; } = default!;
+    public string CodeChallengeMethod { get; init; } = default!;
+    public string? State { get; init; } = default;
+}
