1b1eef77e7b85974e1de5f0991ae5f7677e2e3a7
Richard Garcia <code.richardy@gmail.com>
2026-01-21 23:03:16 -0300

    feat: define redirecturi value object representing a redirect uri address in the domain

---
+++ Source/Vinder.Federation.Domain/Concepts/RedirectUri.cs
@@ -0,0 +1,7 @@
+﻿namespace Vinder.Federation.Domain.Concepts;
+
+public sealed record RedirectUri(string Address) :
+    IValueObject<RedirectUri>
+{
+    public string Address { get; init; } = Address;
+}
