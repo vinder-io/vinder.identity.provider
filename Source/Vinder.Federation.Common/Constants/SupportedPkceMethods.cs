6f8d2eed33db518fa106ee8fcd9757f869485286
Richard Garcia <code.richardy@gmail.com>
2026-01-21 23:39:09 -0300

    feat: introduce supported pkce methods class defining supported pkce methods

---
+++ Source/Vinder.Federation.Common/Constants/SupportedPkceMethods.cs
@@ -0,0 +1,7 @@
+﻿namespace Vinder.Federation.Common.Constants;
+
+public sealed class SupportedPkceMethods
+{
+    public const string PkcePlain = "plain";
+    public const string PkceS256 = "S256";
+}
