2878a2bda3cb7d89bee1178ad2c5ea493b95afdd
Richard Garcia <code.richardy@gmail.com>
2026-01-21 23:51:55 -0300

    feat: define global usings for federation policies and identity handlers and fix usings syntax

--- Source/Vinder.Federation.Infrastructure.IoC/Usings.cs
+++ Source/Vinder.Federation.Infrastructure.IoC/Usings.cs
@@ -8,13 +8,14 @@ global using Microsoft.Extensions.Hosting;
 global using Vinder.Federation.Common.Configuration;
 global using Vinder.Federation.Domain.Collections;
 global using Vinder.Federation.Domain.Aggregates;
+global using Vinder.Federation.Domain.Policies;
 
 global using Vinder.Federation.Application.Services;
 global using Vinder.Federation.Application.Providers;
 global using Vinder.Federation.Application.Payloads.Identity;
 global using Vinder.Federation.Application.Payloads.Authorization;
 global using Vinder.Federation.Application.Payloads.Group;
-global using Vinder.Federation.Application.Payloads.Permission;
+global using Vinder.Federation.Application.Payloads.Permission
 global using Vinder.Federation.Application.Payloads.Tenant;
 global using Vinder.Federation.Application.Payloads.User;
 global using Vinder.Federation.Application.Payloads.Scope;
@@ -26,7 +27,9 @@ global using Vinder.Federation.Application.Validators.Authorization;
 global using Vinder.Federation.Application.Validators.Tenant;
 global using Vinder.Federation.Application.Validators.User;
 global using Vinder.Federation.Application.Validators.Scope;
+
 global using Vinder.Federation.Application.Handlers.Identity;
+global using Vinder.Federation.Application.Policies;
 
 global using Vinder.Federation.Infrastructure.Providers;
 global using Vinder.Federation.Infrastructure.Persistence;
