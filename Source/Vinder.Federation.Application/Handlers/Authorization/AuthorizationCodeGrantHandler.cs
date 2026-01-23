12ac0f6fb221b4bd020c570666728aa3d7546bc1
Richard Garcia <code.richardy@gmail.com>
2026-01-23 17:53:23 -0300

    this commit introduces a refactor of the OAuth authentication flow by delegating processing to grant type–specific handlers, improving maintainability and extensibility

---
+++ Source/Vinder.Federation.Application/Handlers/Authorization/AuthorizationCodeGrantHandler.cs
@@ -0,0 +1,69 @@
+﻿namespace Vinder.Federation.Application.Handlers.Authorization;
+
+public sealed class AuthorizationCodeGrantHandler(ITenantCollection tenantCollection, IUserCollection userCollection, ISecurityTokenService tokenService, ITokenCollection tokenCollection) : IAuthorizationFlowHandler
+{
+    public async Task<Result<ClientAuthenticationResult>> HandleAsync(
+        ClientAuthenticationCredentials parameters, CancellationToken cancellation = default)
+    {
+        var filters = new TokenFiltersBuilder()
+            .WithValue(parameters.Code)
+            .WithType(TokenType.AuthorizationCode)
+            .Build();
+
+        var tokens = await tokenCollection.GetTokensAsync(filters, cancellation: cancellation);
+        var token = tokens.FirstOrDefault();
+
+        if (token is null)
+        {
+            return Result<ClientAuthenticationResult>.Failure(AuthorizationErrors.InvalidAuthorizationCode);
+        }
+
+        if (token.IsExpired)
+        {
+            return Result<ClientAuthenticationResult>.Failure(AuthorizationErrors.AuthorizationCodeExpired);
+        }
+
+        var tenantFilters = new TenantFiltersBuilder()
+            .WithIdentifier(token.TenantId)
+            .Build();
+
+        var tenants = await tenantCollection.GetTenantsAsync(tenantFilters, cancellation: cancellation);
+        var tenant = tenants.FirstOrDefault();
+
+        if (tenant is null)
+        {
+            return Result<ClientAuthenticationResult>.Failure(AuthenticationErrors.ClientNotFound);
+        }
+
+        var codeChallenge = token.Metadata.GetValueOrDefault("code.challenge")!;
+        var codeChallengeMethod = token.Metadata.GetValueOrDefault("code.challenge.method")!;
+
+        if (!PkceCodeVerifier.Validate(parameters.CodeVerifier, codeChallenge, codeChallengeMethod))
+        {
+            return Result<ClientAuthenticationResult>.Failure(AuthorizationErrors.InvalidCodeVerifier);
+        }
+
+        var userFilters = new UserFiltersBuilder()
+            .WithIdentifier(token.UserId)
+            .Build();
+
+        var users = await userCollection.GetUsersAsync(userFilters, cancellation: cancellation);
+        var user = users.FirstOrDefault();
+
+        if (user is null)
+        {
+            return Result<ClientAuthenticationResult>.Failure(AuthenticationErrors.UserNotFound);
+        }
+
+        var tokenResult = await tokenService.GenerateAccessTokenAsync(user, cancellation);
+        if (tokenResult.IsFailure || tokenResult.Data is null)
+        {
+            return Result<ClientAuthenticationResult>.Failure(tokenResult.Error);
+        }
+
+        return Result<ClientAuthenticationResult>.Success(new()
+        {
+            AccessToken = tokenResult.Data.Value
+        });
+    }
+}
